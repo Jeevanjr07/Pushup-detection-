d15c801fd43dad4cbe41753a57bdcd7970d0a363
Jeevanjr07 <76908611+Jeevanjr07@users.noreply.github.com>
2022-04-13 12:31:05 +0530

    Add files via upload

---
+++ push+timer.py
@@ -0,0 +1,88 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from gspread_d import entry
+import time
+from time import perf_counter, sleep
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+def calculate_angle(a,b,c):
+    a = np.array(a) 
+    b = np.array(b) 
+    c = np.array(c) 
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle
+
+cap = cv2.VideoCapture(0)
+counter = 0 
+stage = None
+TIMER  = 5
+a=0
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        results = pose.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        start=perf_counter()
+        try:
+            landmarks = results.pose_landmarks.landmark
+           
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+            angle = calculate_angle(shoulder, elbow, wrist)
+            if angle > 160:
+                stage = "down"
+            if angle < 30 and stage =='down':
+                stage="up"
+                counter +=1 
+                print(counter) 
+                for i in range(15):
+                    sleep(1)
+                end=perf_counter()
+            else:
+                pass
+            
+            
+        except:
+            pass
+        
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+
+        cv2.putText(image, 'REPS', (15,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), 
+                    (10,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, 'STAGE', (65,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                 )               
+        
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+        #3###
+    entry(counter)
+    cap.release()
+    cv2.destroyAllWindows()
+    
